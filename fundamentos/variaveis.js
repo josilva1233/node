1a4830c89f59e2da43f0db9a215320c869c094f6
josias Silva <josilva1233@gmail.com>
2024-06-09 16:34:20 -0300

    Uso de variáveis let or var or const

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,38 @@
+/**
+ * Tutorial Node.js
+ * Variáveis
+ */
+
+console.log("________________")
+//string
+const nome = "Josias Silva"
+console.log(nome)
+console.log(typeof nome)
+//number
+const pi = 3.14
+console.log(pi)
+console.log(typeof pi)
+//boleano
+const vip = true
+console.log(vip)
+console.log(typeof vip)
+//modificação de variaveis
+console.log("________________var")
+var time = "Botafogo"
+console.log(time)
+var time = "Flamengo"
+console.log(time)
+//problema no uso do var para declarar uma variável
+var time =  "Vasco"
+console.log(time)
+//uso do let para não ter problema com redeclaração de variável
+console.log("________________let")
+let linguagem = "PHP"
+console.log(linguagem)
+linguagem = "Javascript"
+console.log(linguagem)
+//let linguagem não pode redeclarar uma variável 
+
+
+
+
