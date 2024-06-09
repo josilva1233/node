727fb705e4f703d278169c3d696282f74773ba6a
josias Silva <josilva1233@gmail.com>
2024-06-09 17:03:55 -0300

    * Operadores:    - Aritméticos    - Aritméticos de atribuição    - Comparação    - Lógicos

---
+++ fundamentos/operadores.js
@@ -0,0 +1,37 @@
+/**
+ * Tutorial Node.js
+ * Operadores:
+   - Aritméticos
+   - Aritméticos de atribuição
+   - Comparação
+   - Lógicos
+ */
+
+console.log("Operadores Aritméticos")
+console.log("+ | some")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("% | módulo (sobra da divisão)")
+console.log("++ | soma 1 ao resultado")
+console.log("-- | subtrai 1 ao resultado")
+console.log("")
+
+
+console.log("Operadores de comparação")
+console.log("> | maior que")
+console.log(">= | maior ou igual a")
+console.log("< | menor que")
+console.log("<= | menor ou igual a")
+console.log("== | igual")
+console.log("!= | diferente")
+console.log("=== | idêntico")
+console.log("!== | não idêntico")
+console.log("")
+
+console.log("Operadores lógicos")
+console.log("&& | AND")
+console.log("|| | OR")
+console.log("! | NOT")
+
+
