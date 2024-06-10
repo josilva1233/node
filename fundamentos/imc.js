061a9e8e4ffd6d079cc1a06d546e37aca7d8a61a
josias Silva <josilva1233@gmail.com>
2024-06-10 15:45:46 -0300

    caculadora IMC

---
+++ fundamentos/imc.js
@@ -0,0 +1,31 @@
+/**
+ * Tutorial Node.js
+ * Entrada de dados via terminal
+ * Aplicativo IMC
+ */
+
+const colors = require('colors')
+const read = require('readline-sync')
+
+console.clear()
+console.log('___ __  __  ___ ')
+console.log('|_ _|  \\/  |/ __|')
+console.log(' | || |\\/| | (__ ')
+console.log('|___|_|  |_|\\___|')
+console.log("")
+let nome = read.question("Digite o seu nome: ")
+let idade = read.question("Digite sua idade: ")
+let peso = Number(read.question("Digite seu peso (kg): ").replace("," , "."))
+let altura = Number(read.question("Digite sua altura (m): ").replace("," , "."))
+let imc = peso / (altura * altura)
+console.log("")
+console.log("__________________________")
+console.log("Ficha do Aluno".italic.bold)
+console.log(`Nome:  ${nome}`)
+console.log(`Idade:  ${idade}`)
+console.log(`Peso:  ${peso}`)
+console.log(`Altura:  ${altura}`)
+console.log(`IMC:  ${imc.toFixed(2)}`)
+
+
+
