41a373e703e55e4b7e548c5e76fda4e276ade612
josias Silva <josilva1233@gmail.com>
2024-06-10 13:52:21 -0300

    Nas estruturas de controle condicional e nos laços de repetições

---
+++ fundamentos/sintaxe2.js
@@ -0,0 +1,22 @@
+/**
+ * Tutorial Node.js
+ * Uso de chaves nas estruturas de controle
+ */
+
+// Nas estruturas de controle condicional e nos laços de repetições
+// Podemos omitir as chaves, porém só uma linha de código é vinculada
+console.log("Sintaxe básica");
+
+let media = 4
+
+if(media < 5)
+{
+   console.log("REPROVADO")
+   console.log("Não emitir certificado")
+}
+
+if(media < 5)
+   console.log("REPROVADO")
+   console.log("Não emitir certificado")
+    
+    
