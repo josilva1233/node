6173549c841f81995d76894d6372538a6ba658c7
josias Silva <josilva1233@gmail.com>
2024-06-13 13:57:24 -0300

    Criando herança dos objetos

--- poo/manicraft.js
+++ poo/manicraft.js
@@ -4,25 +4,48 @@
 
 class Bloco {
     //Atributos
-    constructor (textura, resistencia){
+    constructor(textura, resistencia) {
         this.textura = textura
         this.resistencia = resistencia
     }
     //Ações
-    criarBloco(){
+    criarBloco() {
         console.log("------------------------------------------")
         console.log("┎─┒")
         console.log("┖─┚")
         console.log(`Bloco de ${this.textura}`)
         console.log(`Resistência de ${this.resistencia}`)
     }
-    
-    construir(){
+
+    construir() {
         console.log(`Bloco de ${this.textura} colocado`)
     }
 
-    minerar(){
+    minerar() {
         console.log("▀ ▀ ▀ ▀ Recursos obtidos!")
+    }
+}
+
+class Enxada extends Bloco {
+    //Atributos
+    constructor(textura, resistencia, conquista) {
+        //atributos reutilizados
+        super(textura, resistencia)
+        this.conquista = conquista
+    }
+
+    criarEnxada() {
+        console.log("---------------------------------------------")
+        console.log("-_")
+        console.log(" /")
+        console.log(`Enxada de ${this.textura}`)
+        console.log(`Resistência: ${this.resistencia}`)
+    }
+    arar() {
+        console.log("._._._. Terra arada!")
+        if (this.conquista === true) {
+            console.log(" (^^) S2 Conquista obtida!!!")
+        }
     }
 }
 
@@ -31,7 +54,7 @@ class Bloco {
  */
 
 
-console.clear()   
+console.clear()
 console.log("   _____  .___ _______  ______________________________    _____  ______________________")
 console.log("  /    \\ |   |\\    \\ \\_   _____/\\_   ___ \______  \\  /  _ \\ \\_ _____/\\__ ___/")
 console.log(" / \\ / \\|   |/   |  \\ |    __)_ /    \ \\/|       _/ /  /_\\ \ |    __)    |    |   ")
@@ -50,5 +73,17 @@ bloco2.criarBloco()
 bloco2.construir()
 
 const bloco3 = new Bloco("pedra", 5)
-bloco2.criarBloco()
-bloco2.minerar()
+bloco3.criarBloco()
+bloco3.minerar()
+
+
+const enxada1 = new Enxada("Madeira", 2, false)
+enxada1.criarEnxada()
+enxada1.arar()
+
+const enxada2 = new Enxada("Ferro", 5, true)
+enxada2.criarEnxada()
+enxada2.arar()
+
+const enxada3 = new Enxada("Diamante", 10, false)
+enxada3.criarEnxada()
